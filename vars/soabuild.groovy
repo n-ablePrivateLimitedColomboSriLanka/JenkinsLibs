64697e517be8f7d1b73d7fc2846bd60c4b8159bd
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-03-26 20:03:49 +0530

    modify checkout behavior to clone to a subdir

--- vars/soabuild.groovy
+++ vars/soabuild.groovy
@@ -7,21 +7,24 @@ def call(body) {
 
     pipeline {
         agent { label 'ACE12' }
+        environment {
+           APPLICATION_DEFAULT_DIR='APPLICATION_5JITHW8YDN7KSW'
+        }
         options {
-            skipDefaultCheckout(true)
+           skipDefaultCheckout(true)
         }
         stages {
-            stage('Build') {
-                steps {
-                    cleanWs()
-                    checkout scm
-                    sh '/pipelinescript/build.sh ' + pipelineParams.sharedLib
+           stage('Build') {
+               steps {
+                   cleanWs()
+                   checkout scm: [$class: 'GitSCM', branches: scm.branches, extensions: scm.extensions + [[$class: 'RelativeTargetDirectory', relativeTargetDir: env.APPLICATION_DEFAULT_DIR]], userRemoteConfigs: scm.userRemoteConfigs]
+                   sh '/pipelinescript/build.sh ' + pipelineParams.sharedLib
                 }
             }
 
             stage('Deploy') {
                 steps {
-                    sh '/pipelinescript/deploy.sh ' + "${pipelineParams.nodeSpec} ${pipelineParams.server}"
+                   sh '/pipelinescript/deploy.sh ' + "${pipelineParams.nodeSpec} ${pipelineParams.server}"
                 }
             }
         }
