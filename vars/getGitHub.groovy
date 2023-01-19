c8e7b859484a5c93078c38812856c708229720db
Iresh M Dissanayaka <socialexpz1@gmail.com>
2023-01-19 15:22:37 +0530

    remove files no longer needed; rename soabuild.groovy to acerelease.groovy

--- vars/getGitHub.groovy
+++
@@ -1,6 +0,0 @@
-import org.jenkinsci.plugins.github_branch_source.Connector
-
-def call(def apiUri, credentialsId) {
-    credentials = Connector.lookupScanCredentials(null, apiUri, 'jenkins_github_app', null)
-    return Connector.connect(apiUri, credentials)
-}
